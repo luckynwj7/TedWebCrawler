213dcdbdafcc58cac1a85941946bb7611d298cc2
luckynwj7 <44963879+luckynwj7@users.noreply.github.com>
2020-06-02 13:31:41 +0900

    [사용하기 쉽도록 수정]
    처음 이용하는 사용자가 사용하기 쉽도록 README.txt내용을 수정하였고,
    사용되는 변수들을 input받아서 사용할 수 있도록 구성했습니다.

--- TedWebCrawler/TedWebCrawler.py
+++ TedWebCrawler/TedWebCrawler.py
@@ -3,22 +3,62 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
 import os
+import sys
 
-creatorFile = open("C:\\result\\Creator.wonjong.Noh.txt", 'w', encoding="UTF8")
+creatorFile = open("C:\\result\\1 Creator.wonjong.Noh.txt", 'w', encoding="UTF8")
 creatorFile.close
 
+language = ""
+while True:
+    print ("추출할 언어(한국어:ko, 영어:en)입력")
+    language = input()
+    if language != "en" and language != "ko":
+        print("지원하지 않는 언어입니다. 다시 입력해주세요.")
+    else:
+        break
+
+startPage = 1
+endPage = 2
+while True:
+    print ("검색을 시작할 페이지 입력")
+    startPage = input()
+    if int(startPage) < 1:
+        print("0보다 큰 정수를 입력하세요.")
+    else:
+        break
+
+while True:
+    print("검색을 종료할 페이지 입력")
+    endPage = input()
+    if int(endPage) < int(startPage):
+        print("범위를 초과하였습니다. 다시 입력해주세요. 현재 시작 페이지는 " + startPage + "입니다.")
+    else:
+        break
+
+print("로딩 지연 시간 입력(정수로 입력하세요. 2초가 적당하며, PC의 인터넷 연결 상태에 따라 결과가 달라질 수 있습니다)")
+sleepTime = input()
+
+
+
 currentPath = os.path.dirname( os.path.abspath( __file__ ) )
-driver = webdriver.Chrome(currentPath + '\chromedriver.exe')
+print("실행 파일 경로 : " + currentPath)
+try:
+    driver = webdriver.Chrome(currentPath + '\chromedriver.exe')
+except:
+    print("실행 파일 경로 내에 버전에 맞는 크롬 드라이버가 없어 10초 후 실행을 종료합니다.")
+    sleep(10)
+    sys.exit()
 
 """1페이지부터 106페이지까지"""
 serverNum = 1
-for serverNum in range(1,106):
+for serverNum in range(int(startPage), int(endPage)):
     tempUrl = "https://www.ted.com/talks?language=ko&page=" + str(serverNum)
 
     html = urlopen(tempUrl)
     bsObject = BeautifulSoup(html, "html.parser")
     bsObject = bsObject.find_all('div', class_='talk-link')
     findLink = []
+    errorCount = 1
 
     for link in bsObject:
         strObject = str(link)
@@ -27,14 +67,21 @@ for serverNum in range(1,106):
         startPosition = strObject.find("href")
         endPosition = strObject.find("language=ko")
         strObject = strObject[startPosition+6:endPosition-1]
-        strObject = "https://www.ted.com" + strObject + "/transcript?language=en"
+        strObject = "https://www.ted.com" + strObject + "/transcript?language=" + language
         findLink.append(strObject)
 
     for inLink in findLink:
         print("-------------------------구분자-------------------------")
         """링크 내에서 제목 찾기"""
         testObj = inLink
-        html = urlopen(testObj)
+        try:
+            html = urlopen(testObj)
+        except:
+            exceptFile = open("C:\\result\\2 linkErrorFile " + str(errorCount) + "(page" + str(serverNum) +").txt", 'w', encoding="UTF8")
+            exceptFile.write(inLink)
+            errorCount += 1
+            exceptFile.close
+            continue
         bsObject = BeautifulSoup(html, "html.parser")
         bsObject = bsObject.find_all('title')
         strObject = str(bsObject)
@@ -42,7 +89,30 @@ for serverNum in range(1,106):
         endPosition = strObject.find("TED Talk")
         strObject = strObject[startPosition+2:endPosition-3]
         titleName = strObject
+        if titleName == "" :
+            exceptFile = open("C:\\result\\2 titleNameErrorFile " + str(errorCount) + "(page" + str(serverNum) +").txt", 'w', encoding="UTF8")
+            exceptFile.write(inLink)
+            errorCount += 1
+            exceptFile.close
+            continue
+
+        """이름으로 지을 수 없는 타이틀 내용 지우기"""
+        titleName = titleName.replace("\\","")
+        titleName = titleName.replace("/","")
+        titleName = titleName.replace("?","")
+        titleName = titleName.replace("|","")
+        titleName = titleName.replace("*","")
+        titleName = titleName.replace('"',"")
+        titleName = titleName.replace('<',"")
+        titleName = titleName.replace('>',"")
+        titleName = titleName.replace(':',"")
+
         print(titleName + "\n")
+        if os.path.isfile("C:\\result\\" + titleName + ".txt") :
+            print(titleName + "가 이미 존재함")
+            continue
+
+        savePath = "C:\\result\\" + titleName + ".txt"
 
 
         """링크 내에서 내용 찾기"""
@@ -72,7 +142,7 @@ for serverNum in range(1,106):
         timeList = []
         testObj = inLink
         driver.get(testObj)
-        sleep(2)
+        sleep(int(sleepTime))
         html = driver.page_source
         bsObject = BeautifulSoup(html, 'html.parser')
         bsObject = bsObject.select('button')
@@ -90,29 +160,30 @@ for serverNum in range(1,106):
         """컨텐츠 합치기"""
         count = 0
         result = ""
+        indexErrorFlag = False
         for text in lastStrList:
-            result += timeList[count]
+            try:
+                result += timeList[count]
+            except:
+                IndexErrorFlag = True
+                break
             result += "\n"
             result += text
             result += "\n\n"
             count +=1
+
+        if indexErrorFlag :
+            exceptFile = open("C:\\result\\2 timeErrorFile " + str(errorCount) + "(page" + str(serverNum) +").txt", 'w', encoding="UTF8")
+            exceptFile.write(inLink)
+            errorCount += 1
+            exceptFile.close
+            indexErrorFlag = False
+            continue
         
         print(result)
 
-        """이름으로 지을 수 없는 타이틀 내용 지우기"""
-        titleName = titleName.replace("\\","")
-        titleName = titleName.replace("/","")
-        titleName = titleName.replace("?","")
-        titleName = titleName.replace("|","")
-        titleName = titleName.replace("*","")
-        titleName = titleName.replace('"',"")
-        titleName = titleName.replace('<',"")
-        titleName = titleName.replace('>',"")
-        titleName = titleName.replace(':',"")
 
 
-
-        savePath = "C:\\result\\" + titleName + ".txt"
         f = open(savePath, 'w', encoding="UTF8")
         f.write(result)
         f.close
